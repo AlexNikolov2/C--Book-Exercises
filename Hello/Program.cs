3c9a6509be2ba4551e9ddc4d45c33f84ffbaf848
AlexNikolov2 <aletoooking00@gmail.com>
2022-11-11 23:02:30 +0200

    expresssion, exercise 2

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,9 +4,9 @@ namespace Hello
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello C#");
+            Console.WriteLine((3522 + 52353) * 23 - (2336 *501 + 23432 - 6743) * 3 );
         }
     }
 }
